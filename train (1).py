ddfa3d31ca867a92f4c9618c3631d1b9ac020919
Yagna27 <68897553+Yagna27@users.noreply.github.com>
2021-04-15 16:22:34 +0530

    Update train (1).py

--- train (1).py
+++ train (1).py
@@ -41,7 +41,7 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    run.log("average_precision_score_weighted", np.float(accuracy))
 
 if __name__ == '__main__':
     main()
