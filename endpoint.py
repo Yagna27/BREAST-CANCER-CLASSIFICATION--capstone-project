69ab403fce9720e512d9e13b21aab0353bd1f656
Yagna27 <yagnasrip2@gmail.com>
2021-04-17 10:32:48 +0000

    completed

---
+++ endpoint.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+# URL for the web service, should be similar to:
+# 'http://42e7f579-4b3d-4250-aee7-702d67e48ee5.southcentralus.azurecontainer.io/score'
+scoring_uri = 'http://42e7f579-4b3d-4250-aee7-702d67e48ee5.southcentralus.azurecontainer.io/score'
+# If the service is authenticated, set the key or token
+key = 'D0zGpcSmGMdVKuDLbKtPFoIZUwehwyYT'
